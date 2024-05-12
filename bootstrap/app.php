18761469a4c000def61338f95092882ddfba04af
Prakash Kushwaha <prakash.181529@ncit.edu.np>
2024-05-12 18:00:55 +0545

    fully working auth system

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         
+        
+        $middleware->alias([
+            'admin.guest' => App\Http\Middleware\AdminRedirect::class,
+            'admin.auth' => App\Http\Middleware\AdminAuthenticate::class,
+        ]);
+
+
+
         $middleware->redirectTo(
             guests: '/account/login',
             users: '/account/dashboard',  
